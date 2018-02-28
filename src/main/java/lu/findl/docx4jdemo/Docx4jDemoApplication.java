791cb718f96a1efa2cc71e14c729e83dd8a645d9
Houssam Eddine RAFIKI <houssam-eddine.rafiki@findl.lu>
2018-02-28 16:56:05 +0000

    First commit

---
+++ src/main/java/lu/findl/docx4jdemo/Docx4jDemoApplication.java
@@ -0,0 +1,12 @@
+package lu.findl.docx4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Docx4jDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Docx4jDemoApplication.class, args);
+	}
+}
