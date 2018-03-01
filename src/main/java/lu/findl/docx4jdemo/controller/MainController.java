06ce5bd24ca58ad09f0ca76b21d9ba7d32385786
Houssam Eddine RAFIKI <houssam-eddine.rafiki@findl.lu>
2018-03-01 10:24:52 +0000

    Converting xlsx to pdf work

--- src/main/java/lu/findl/docx4jdemo/controller/MainController.java
+++ src/main/java/lu/findl/docx4jdemo/controller/MainController.java
@@ -1,17 +1,62 @@
 package lu.findl.docx4jdemo.controller;
 
+import lu.findl.docx4jdemo.helper.XLStoPDF;
 import org.docx4j.Docx4J;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.SpreadsheetMLPackage;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.PartName;
+import org.docx4j.openpackaging.parts.SpreadsheetML.WorksheetPart;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.xlsx4j.jaxb.Context;
+import org.xlsx4j.sml.*;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
 
 @RestController
 public class MainController {
+
+    private static void addContent(WorksheetPart sheet) {
+
+        // Minimal content already present
+        SheetData sheetData = sheet.getJaxbElement().getSheetData();
+
+        // Now add
+        Row row = Context.getsmlObjectFactory().createRow();
+        //row.setR((long) 1);  // optional
+
+        Cell cell = Context.getsmlObjectFactory().createCell();
+        cell.setV("1234");
+        cell.setR("A1");  // Apple Numbers needs this, or cell content won't show
+        row.getC().add(cell);
+
+        Cell cell2 = createCell("hello world!");
+        row.getC().add(cell2);
+        cell2.setR("B1"); // be careful the numeral matches the row correctly, or Excel will complain
+
+        sheetData.getRow().add(row);
+    }
+
+    private static Cell createCell(String content) {
+
+        Cell cell = Context.getsmlObjectFactory().createCell();
+
+        CTXstringWhitespace ctx = Context.getsmlObjectFactory().createCTXstringWhitespace();
+        ctx.setValue(content);
+
+        CTRst ctrst = new CTRst();
+        ctrst.setT(ctx);
+
+        cell.setT(STCellType.INLINE_STR);
+        cell.setIs(ctrst); // add ctrst as inline string
+
+        return cell;
+
+    }
 
     @RequestMapping("/docx")
     public String handlingDocx() {
@@ -21,17 +66,18 @@ public class MainController {
             wordMLPackage = WordprocessingMLPackage.createPackage();
             MainDocumentPart mdp = wordMLPackage.getMainDocumentPart();
             mdp.addParagraphOfText("hello world");
-            String filename = System.getProperty("user.dir") + "\\OUT_hello.docx";
+            String filename = System.getProperty("user.dir") + "\\OUT_hello_doc.docx";
             Docx4J.save(wordMLPackage, new java.io.File(filename), Docx4J.FLAG_SAVE_ZIP_FILE);
-            System.out.println("Saved " + filename);
+            System.out.println("DOCX Saved .." + filename);
 
             // Convertion du document générer en pdf
-            String outputfilepath = System.getProperty("user.dir") + "/OUT_hello.pdf";
+            String outputfilepath = System.getProperty("user.dir") + "\\OUT_hello_doc.pdf";
             OutputStream os = new java.io.FileOutputStream(outputfilepath);
             Docx4J.toPDF(wordMLPackage, os);
-            System.out.println("Converted " + outputfilepath);
+            os.close();
+            System.out.println("DOCX Converted .." + outputfilepath);
 
-        } catch (Docx4JException | FileNotFoundException e) {
+        } catch (Docx4JException | IOException e) {
             e.printStackTrace();
         }
 
@@ -40,8 +86,23 @@ public class MainController {
 
     @RequestMapping("/xlsx")
     public String handlingXlsx() {
-        WordprocessingMLPackage wordMLPackage = null;
+        try {
+            // Exemple de creation d'un fichier xlsx
+            String outputfilepath = System.getProperty("user.dir") + "\\OUT_hello_xsl.xlsx";
+            SpreadsheetMLPackage pkg = SpreadsheetMLPackage.createPackage();
+            WorksheetPart sheet = pkg.createWorksheetPart(new PartName("/xl/worksheets/sheet1.xml"), "Sheet1", 1);
+            //ajout d'un contenu dans le fichier xlsx (deux cellules 1234 et Hello world)
+            addContent(sheet);
+            pkg.save(new File(outputfilepath));
+            System.out.println("XSLX Saved .." + outputfilepath);
 
+            //Converting xlsx to pdf using itext
+            String outputpdffilepath = System.getProperty("user.dir") + "\\OUT_hello_xsl.pdf";
+            XLStoPDF.convert(outputfilepath, outputpdffilepath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return "Greetings from Spring Boot!";
     }
